d3384031d7d59687bfe9e44826e81cfe96cf511c
cms11231 <44215786+cms11231@users.noreply.github.com>
2019-11-05 08:21:54 -0700

    Update moneygame.rb
    Fix big

--- moneygame.rb
+++ moneygame.rb
@@ -4,66 +4,7 @@ module ZOrder
     BACKGROUND, STARS, PLAYER, UI = *0..3
 end
 
-class Tutorial < Gosu::Window
-    def initialize
-        super 640, 480
-        self.caption = "Tutorial Game"
-  
-        @background_image = Gosu::Image.new("media/land.jpg", :tileable => true)
-  
-        @player = Player.new
-        @player.warp(320, 240)
 
-        @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
-        @stars = Array.new
-        @bigs = Array.new
-        @font = Gosu::Font.new(20)
-    end
-  
-    def update
-        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
-            @player.go_left
-        end
-        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
-            @player.go_right
-        end
-        if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
-            @player.accelerate
-        end
-        @player.move
-        @player.collect_stars(@stars)
-
-        if rand(100) < 2 and @stars.size < 10
-            @stars.push(Star.new(@star_anim))
-        end
-
-        if rand(100) < 1 and @stars.size < 2
-            @stars.push(Star.new(@star_anim))
-        end
-    end
-  
-    def draw
-        @player.draw
-        @background_image.draw(0, 0, 0)
-        @stars.each { |star| 
-        star.draw
-        star.move
-        }
-        @bigs.each { |big| 
-        big.draw
-        big.move
-        }
-        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
-    end
-  
-    def button_down(id)
-        if id == Gosu::KB_ESCAPE
-            close
-        else
-            super
-        end
-    end
-end
 
 
 class Player
@@ -120,6 +61,18 @@ class Player
             end
         end
     end
+
+    def collect_bigs(bigs)
+        bigs.reject! do |big|
+            if Gosu.distance(@x, @y, big.x, big.y) < 35
+                @score += 50
+                @beep.play
+                true
+            else
+                false
+            end
+        end
+    end
 end
 
 class Star
@@ -159,5 +112,70 @@ class Big
         @image.draw(@x, @y, ZOrder::STARS)    
     end
 end
+
+class Tutorial < Gosu::Window
+    def initialize
+        super 640, 480
+        self.caption = "Tutorial Game"
+  
+        @background_image = Gosu::Image.new("media/land.jpg", :tileable => true)
+  
+        @player = Player.new
+        @player.warp(320, 240)
+
+        @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
+        @stars = Array.new
+        @bigs = Array.new
+        @font = Gosu::Font.new(20)
+    end
+  
+    def update
+        if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+            @player.go_left
+        end
+        if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+            @player.go_right
+        end
+        if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+            @player.accelerate
+        end
+        @player.move
+        @player.collect_stars(@stars)
+        @player.collect_bigs(@bigs)
+
+        if rand(100) < 2 and @stars.size < 10
+            @stars.push(Star.new(@star_anim))
+        end
+
+        if rand(100) < 10 and @bigs.size < 2
+            @bigs.push(Big.new)
+        end
+    end
+  
+    def draw
+        @player.draw
+        @background_image.draw(0, 0, 0)
+        @stars.each { |star| 
+        star.draw
+        star.move
+        }
+        @bigs.each { |big| 
+        big.draw
+        big.move
+        }
+        @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+    end
+  
+    def button_down(id)
+        if id == Gosu::KB_ESCAPE
+            close
+        else
+            super
+        end
+    end
+end
+
+
+
 
 Tutorial.new.show
